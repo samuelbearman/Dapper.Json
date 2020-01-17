11d171cf03eb82cd40ab181ea63e371a112f72c9
Samuel Bearman <samuel.opensource@gmail.com>
2020-01-17 16:51:59 -0500

    Basic One-To-One set up

--- src/Dapper.Json/QueryBuilder/RawSql.cs
+++ src/Dapper.Json/QueryBuilder/RawSql.cs
@@ -1,69 +1,172 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Dapper.Json.QueryBuilder
 {
     public static class RawSql
     {
-
-        public static string Build<T>(T template)
+        public static string Build<T>(T template, bool isAggregate = false)
         {
             // Assume being passed the Aggreagte 
-            List<string> withOneToMany = new List<string>();
-            List<string> withOneToOne = new List<string>();
+            List<QueryObject> withOneToMany = new List<QueryObject>();
+            List<QueryObject> withOneToOne = new List<QueryObject>();
+
+            string result = "";
 
             var properties = typeof(T).GetProperties();
-            foreach(var property in properties)
+            foreach (var property in properties)
             {
                 if (property.Name.Contains("Id") && property.Name != typeof(T).Name + "Id")
                 {
+                    // One To One Id
+                    var childClass = properties.SingleOrDefault(x => x.Name == property.Name.Remove(property.Name.Length - 2) && x.GetType().IsClass);
 
-                    withOneToOne.Add(property.Name);
+                    var childProperties = childClass.PropertyType.GetProperties();
+                    string nestedQuery = "";
+
+                    foreach (var childProperty in childProperties)
+                    {
+                        nestedQuery = Build(childProperty);
+                    }
+
+                    string parentAlias = string.Concat(typeof(T).Name.Where(c => c >= 'A' && c <= 'Z'));
+                    string childAlias = string.Concat(property.Name.Remove(property.Name.Length - 2).Where(c => c >= 'A' && c <= 'Z'));
+
+                    var queryObj = new QueryObject()
+                    {
+                        ChildAliasName = childAlias,
+                        ParentAliasName = parentAlias,
+                        ChildTableName = property.Name.Remove(property.Name.Length - 2) + "s",
+                        JoiningColumnName = property.Name,
+                        ParentPropertyName = property.Name.Remove(property.Name.Length - 2),
+                    };
+
+                    withOneToOne.Add(queryObj);
+
+                    result = OneToOneQueryBlock(queryObj, nestedQuery);
                 }
 
-                if(property.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(property.PropertyType))
-                {
-                    withOneToMany.Add(property.Name);
-                }
+                //if (property.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(property.PropertyType))
+                //{
+                //    // One To Many Id
+                //    var childClass = property.PropertyType.GetGenericArguments()[0];
+                //    //var childClass = properties.SingleOrDefault(x => x.Name == property.Name && x.GetType().IsClass);
+
+                //    var childProperties = childClass.GetProperties();
+                //    string nestedQuery = "";
+
+                //    foreach (var childProperty in childProperties)
+                //    {
+                //        nestedQuery = Build(childProperty);
+                //    }
+
+                //    string parentAlias = string.Concat(typeof(T).Name.Where(c => c >= 'A' && c <= 'Z'));
+                //    string childAlias = string.Concat(property.Name.Remove(property.Name.Length - 2).Where(c => c >= 'A' && c <= 'Z'));
+
+                //    var queryObj = new QueryObject()
+                //    {
+                //        ChildAliasName = childAlias,
+                //        ParentAliasName = parentAlias,
+                //        ChildTableName = property.Name.Remove(property.Name.Length - 2) + "s",
+                //        JoiningColumnName = property.Name,
+                //        ParentPropertyName = property.Name.Remove(property.Name.Length - 2),
+                //    };
+
+                //    withOneToMany.Add(queryObj);
+
+                //    result = OneToManyQueryBlock(queryObj, nestedQuery);
+                //}
             }
 
-            return "";
+            if (isAggregate)
+            {
+                string tableAlias = string.Concat(typeof(T).Name.Where(c => c >= 'A' && c <= 'Z'));
+                result = AggregateQueryBlock(typeof(T).Name + "s", tableAlias, result);
+            }
+
+            return result;
         }
 
-        private static string OneToOneQueryBlock(string name)
+        private static string AggregateQueryBlock(string tableName, string tableAlias, string nestedBlocks)
         {
-            // 0: actual sql table name
-            // 1: sql alias of the table
-            // 2: parent alias of the joining table
-            // 3: joining column name
-            // 4: name of the parent property's property 
-            string childTableName = "BucketMappings";
-            string childTableAlias = "BM";
-            string parentTableAlias = "BD";
-            string joiningColumnName = "BucketMappingId";
-            string parentPropertyName = "BucketMapping";
+            string aggregateQuery;
 
+            if (string.IsNullOrWhiteSpace(nestedBlocks))
+            {
+                aggregateQuery = $@"
+                select {tableAlias}.*
+                from {tableName} {tableAlias}
+			    for 
+                    json path, without_array_wrapper";
+            }
+            else
+            {
+                aggregateQuery = $@"
+                select {tableAlias}.*,
+			    {nestedBlocks}
+                from {tableName} {tableAlias}
+			    for 
+                    json path, without_array_wrapper";
+            }
 
-            var oneToOnequery = $@"json_query((
-				select {childTableAlias}.*
-				from {childTableName} {childTableAlias}
-				where {parentTableAlias}.{joiningColumnName} = {childTableAlias}.{joiningColumnName}
-				for json path, without_array_wrapper
-			)) as [{parentPropertyName}]";
-
-            return oneToOnequery;
+            return aggregateQuery;
         }
 
-        private static string OneToOneQueryBlock(QueryObject query)
+        private static string OneToManyQueryBlock(QueryObject query, string nestedBlock)
         {
-            var oneToOnequery = $@"json_query((
-				select {query.ChildTableName}.*
+            string oneToManyQuery;
+
+            if (string.IsNullOrWhiteSpace(nestedBlock))
+            {
+                oneToManyQuery = $@"json_query((
+                select {query.ChildAliasName}.*
+			    from {query.ChildTableName} {query.ChildAliasName}
+			    where {query.ParentAliasName}.{query.JoiningColumnName} = {query.ChildAliasName}.{query.JoiningColumnName}
+			    for json path
+                )) as [{query.ParentPropertyName}]";
+            }
+            else
+            {
+                oneToManyQuery = $@"json_query((
+                select {query.ChildAliasName}.*,
+                {nestedBlock}
+			    from {query.ChildTableName} {query.ChildAliasName}
+			    where {query.ParentAliasName}.{query.JoiningColumnName} = {query.ChildAliasName}.{query.JoiningColumnName}
+			    for json path
+                )) as [{query.ParentPropertyName}]";
+            }
+
+            return oneToManyQuery;
+        }
+
+        private static string OneToOneQueryBlock(QueryObject query, string nestedBlock)
+        {
+            string oneToOneQuery;
+
+            if (string.IsNullOrWhiteSpace(nestedBlock))
+            {
+                oneToOneQuery = $@"json_query((
+				select {query.ChildAliasName}.*
 				from {query.ChildTableName} {query.ChildAliasName}
 				where {query.ParentAliasName}.{query.JoiningColumnName} = {query.ChildAliasName}.{query.JoiningColumnName}
 				for json path, without_array_wrapper
-			)) as [{query.ParentPropertyName}]";
+			    )) as [{query.ParentPropertyName}]";
+            }
+            else
+            {
+                oneToOneQuery = $@"json_query((
+				select {query.ChildAliasName}.*,
+                {nestedBlock}
+				from {query.ChildTableName} {query.ChildAliasName}
+				where {query.ParentAliasName}.{query.JoiningColumnName} = {query.ChildAliasName}.{query.JoiningColumnName}
+				for json path, without_array_wrapper
+			    )) as [{query.ParentPropertyName}]";
+            }
 
-            return oneToOnequery;
+            return oneToOneQuery;
         }
     }
 }
---
+++ tests/Dapper.Json.UnitTests/Dapper.Json.Tests/RawSqlTests.cs
@@ -0,0 +1,68 @@
+﻿using Dapper.Json.QueryBuilder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Dapper.Json.Tests
+{
+    public class RawSqlTests
+    {
+        public class Parent
+        {
+            public Parent()
+            {
+                ParentId = 1;
+                Description = "Test";
+                Children = new List<Child>()
+                {
+                    new Child()
+                    {
+                        ChildId = 45,
+                        Description = "This is a test"
+                    },
+                    new Child()
+                    {
+                        ChildId = 48,
+                        Description = "This is a test"
+                    }
+                };
+                Sibling = new Sibling()
+                {
+                    SiblingId = 78,
+                    Description = "Sibling Test",
+                };
+            }
+
+            public int ParentId { get; set; }
+            public string Description { get; set; }
+            public List<Child> Children { get; set; }
+            public int SiblingId { get; set; }
+            public Sibling Sibling { get; set; }
+
+        }
+        public class Sibling
+        {
+            public int SiblingId { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class Child
+        {
+            public int ChildId { get; set; }
+            public string Description { get; set; }
+        }
+
+        [Fact]
+        public void ShouldReturn()
+        {
+            Parent testClass = new Parent();
+
+            //var intTets = RawSql.Build(2);
+
+            var result = RawSql.Build(testClass, true);
+
+            var test = "";
+        }
+    }
+}
