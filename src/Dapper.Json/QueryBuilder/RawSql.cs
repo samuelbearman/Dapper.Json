642e83fe47e3e5430fd31f0deaefb57fffc9bb41
Samuel Bearman <samuel.opensource@gmail.com>
2020-01-18 01:30:31 -0500

    Many to many basic

--- src/Dapper.Json/QueryBuilder/RawSql.cs
+++ src/Dapper.Json/QueryBuilder/RawSql.cs
@@ -2,21 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace Dapper.Json.QueryBuilder
 {
     public static class RawSql
     {
-        public static string Build<T>(T template, bool isAggregate = false)
+        public static string Build<T>(T template, bool isAggregate = false, bool IsPrimitive = false)
         {
             // Assume being passed the Aggreagte 
             List<QueryObject> withOneToMany = new List<QueryObject>();
             List<QueryObject> withOneToOne = new List<QueryObject>();
 
             string result = "";
+            PropertyInfo[] properties = null;
 
-            var properties = typeof(T).GetProperties();
+            if (IsPrimitive)
+                properties = new PropertyInfo[] { };
+            else
+                properties = typeof(T).GetProperties();
+
             foreach (var property in properties)
             {
                 if (property.Name.Contains("Id") && property.Name != typeof(T).Name + "Id")
@@ -29,7 +35,8 @@ namespace Dapper.Json.QueryBuilder
 
                     foreach (var childProperty in childProperties)
                     {
-                        nestedQuery = Build(childProperty);
+                        bool isPrimitive = IsSimple(childProperty.PropertyType);
+                        nestedQuery = Build(childProperty.PropertyType, false, isPrimitive);
                     }
 
                     string parentAlias = string.Concat(typeof(T).Name.Where(c => c >= 'A' && c <= 'Z'));
@@ -46,39 +53,39 @@ namespace Dapper.Json.QueryBuilder
 
                     withOneToOne.Add(queryObj);
 
-                    result = OneToOneQueryBlock(queryObj, nestedQuery);
+                    result = OneToOneQueryBlock(queryObj, nestedQuery, result);
                 }
 
-                //if (property.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(property.PropertyType))
-                //{
-                //    // One To Many Id
-                //    var childClass = property.PropertyType.GetGenericArguments()[0];
-                //    //var childClass = properties.SingleOrDefault(x => x.Name == property.Name && x.GetType().IsClass);
+                if (property.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(property.PropertyType))
+                {
+                    // One To Many
+                    var collectionType = property.PropertyType.GetGenericArguments()[0];
 
-                //    var childProperties = childClass.GetProperties();
-                //    string nestedQuery = "";
+                    var collectionTypeProperties = collectionType.GetProperties();
+                    string nestedQuery = "";
 
-                //    foreach (var childProperty in childProperties)
-                //    {
-                //        nestedQuery = Build(childProperty);
-                //    }
+                    foreach (var childProperty in collectionTypeProperties)
+                    {
+                        bool isPrimitive = IsSimple(childProperty.PropertyType);
+                        nestedQuery = Build(childProperty.PropertyType, false, isPrimitive);
+                    }
 
-                //    string parentAlias = string.Concat(typeof(T).Name.Where(c => c >= 'A' && c <= 'Z'));
-                //    string childAlias = string.Concat(property.Name.Remove(property.Name.Length - 2).Where(c => c >= 'A' && c <= 'Z'));
+                    string parentAlias = string.Concat(typeof(T).Name.Where(c => c >= 'A' && c <= 'Z'));
+                    string childAlias = string.Concat(property.Name.Remove(property.Name.Length - 2).Where(c => c >= 'A' && c <= 'Z'));
 
-                //    var queryObj = new QueryObject()
-                //    {
-                //        ChildAliasName = childAlias,
-                //        ParentAliasName = parentAlias,
-                //        ChildTableName = property.Name.Remove(property.Name.Length - 2) + "s",
-                //        JoiningColumnName = property.Name,
-                //        ParentPropertyName = property.Name.Remove(property.Name.Length - 2),
-                //    };
+                    var queryObj = new QueryObject()
+                    {
+                        ChildAliasName = childAlias,
+                        ParentAliasName = parentAlias,
+                        ChildTableName = property.Name,
+                        JoiningColumnName = typeof(T).Name + "Id",
+                        ParentPropertyName = property.Name,
+                    };
 
-                //    withOneToMany.Add(queryObj);
+                    withOneToMany.Add(queryObj);
 
-                //    result = OneToManyQueryBlock(queryObj, nestedQuery);
-                //}
+                    result = OneToManyQueryBlock(queryObj, nestedQuery, result);
+                }
             }
 
             if (isAggregate)
@@ -88,6 +95,20 @@ namespace Dapper.Json.QueryBuilder
             }
 
             return result;
+        }
+
+        public static bool IsSimple(Type type)
+        {
+            var typeInfo = type.GetTypeInfo();
+            if (typeInfo.IsGenericType && typeInfo.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                // nullable type, check if the nested type is simple.
+                return IsSimple(typeInfo.GetGenericArguments()[0]);
+            }
+            return typeInfo.IsPrimitive ||
+                typeInfo.IsEnum ||
+                type.Equals(typeof(string)) ||
+                type.Equals(typeof(decimal));
         }
 
         private static string AggregateQueryBlock(string tableName, string tableAlias, string nestedBlocks)
@@ -115,7 +136,7 @@ namespace Dapper.Json.QueryBuilder
             return aggregateQuery;
         }
 
-        private static string OneToManyQueryBlock(QueryObject query, string nestedBlock)
+        private static string OneToManyQueryBlock(QueryObject query, string nestedBlock, string curentResult)
         {
             string oneToManyQuery;
 
@@ -139,10 +160,16 @@ namespace Dapper.Json.QueryBuilder
                 )) as [{query.ParentPropertyName}]";
             }
 
+            if (!string.IsNullOrWhiteSpace(curentResult))
+            {
+                curentResult += "," + oneToManyQuery;
+                return curentResult;
+            }
+
             return oneToManyQuery;
         }
 
-        private static string OneToOneQueryBlock(QueryObject query, string nestedBlock)
+        private static string OneToOneQueryBlock(QueryObject query, string nestedBlock, string curentResult)
         {
             string oneToOneQuery;
 
@@ -164,6 +191,12 @@ namespace Dapper.Json.QueryBuilder
 				where {query.ParentAliasName}.{query.JoiningColumnName} = {query.ChildAliasName}.{query.JoiningColumnName}
 				for json path, without_array_wrapper
 			    )) as [{query.ParentPropertyName}]";
+            }
+
+            if (!string.IsNullOrWhiteSpace(curentResult))
+            {
+                curentResult += "," + oneToOneQuery;
+                return curentResult;
             }
 
             return oneToOneQuery;
